6e968e4322a1c48897f93cbc354e294b903ca970
NAKASHIMA, Makoto <makoto.nksm+github@gmail.com>
2020-07-28 23:40:40 +0900

    Bump version to v0.1.1

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Add this to your `Cargo.toml`:
 
 ```toml
 [dependencies]
-etc-passwd = "0.1.0"
+etc-passwd = "0.1.1"
 ```
 
 ## Examples
--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! etc-passwd = "0.1.0"
+//! etc-passwd = "0.1.1"
 //! ```
 //!
 //! # Examples
@@ -36,7 +36,7 @@
 //! [`getpwnam_r(3)`]: https://man7.org/linux/man-pages/man3/getpwnam_r.3.html
 //! [`getpwuid_r(3)`]: https://man7.org/linux/man-pages/man3/getpwuid_r.3.html
 
-#![doc(html_root_url = "https://docs.rs/etc-passwd/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/etc-passwd/0.1.1")]
 #![warn(missing_docs)]
 #![warn(unused)]
 #![warn(unused_crate_dependencies)]
